f30c0168afc38673929906cbafed85ffc4a47af3
JakeMennes <Mennesjr@dukes.jmu.edu>
2023-01-27 11:37:46 -0500

    Draft work

--- Module 1/2 - Tutorials/1.2.0 - Operators_Tutorial.py
+++ Module 1/2 - Tutorials/1.2.0 - Operators_Tutorial.py
@@ -1,27 +1,26 @@
-# !! use an import here for math !!
-
+# Use Math here
+import math
 # Let's run through some simple operations:
 
 # First, let's go through the steps to find the magnitude
 # of two vectors:
-
+# Define the vectors
 vector_one = 16
+print("Vector 1 = " + str(vector_one))
 vector_two = 23
+print("Vector 2 = " + str(vector_two))
 
 # First, let's square each vector. Use whichever method you like:
-one_squared = -1
-two_squared = -1
+vector_one_squared = math.pow(vector_one, 2)
+print("Vector 1 squared = " + str(vector_one_squared))
+vector_two_squared = math.pow(vector_two, 2)
+print("Vector 2 squared = " + str(vector_two_squared))
 
 # Next, let's add the two vectors together:
-vector_sum = -1
+Added = vector_one_squared + vector_two_squared
+print("The two vectors added = " + str(Added))
 
 # Finally, find the square root of the sum. Use any method:
-magnitude = -1
+Squared = math.sqrt(Added)
+print("The added vectors squared = " + str(Squared))
 
-# Use a print statement to check your work!
-# Answer should be around 28.
-print()
-
-# Now, try to do that process all in one step!
-magnitude_two = -1
-print()
--- Module 1/3 - Assignments/1.3.0 - Operators/Operators.py
+++ Module 1/3 - Assignments/1.3.0 - Operators/Operators.py
@@ -6,18 +6,29 @@
 # print(first_number)
 
 # multiply two factors of 64
-first_number = None
+first_number = 8 * 8
+print("First Number = " + str(first_number))
 
 # divide first_number by 10
-second_number = None
+second_number = first_number / 10
+print("Second Number = " + str(second_number))
 
 # add second_number to first_number, then subtract 2
-third_number = None
+third_number = ((first_number + second_number) - 2)
+print("Third Number = " + str(third_number))
 
 # now, using parentheses, divide second_number by 2, then multiply it by 20, and finally add 2.4
-fourth_number = None
+fourth_number = (((second_number / 2) * 20) + 2.4)
+print("Forth Number = " + str(fourth_number))
 
 # think of two different ways to raise 8 to the 2nd power
 # hint, for one of them you have to import a module discussed in class
-squared_number_one = None
-squared_number_two = None
+# import math here
+import math
+
+squared_number_one = math.pow(8, 2)
+print("8 squared is: " + str(squared_number_one))
+
+squared_number_two = (8 ** 2)
+print("8 squared is: " + str(squared_number_two))
+
